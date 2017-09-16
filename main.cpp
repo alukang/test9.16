7ced9b8fffb7980d72d308d7c41da7bf2e4738a3
alukang <1472395315@qq.com>
2017-09-16 16:52:37 +0800

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include <string>
+#include <unistd.h>
+
+using namespace std;
+int main()
+{
+    cout<<"hello world"<<endl;
+    cout<<"ni hao"<<endl;
+    cout <<"-------------水-------------"<<endl;
+    return 0;
+}
